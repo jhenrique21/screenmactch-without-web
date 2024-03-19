cf232b49b8431a15f56da6ef2845d6be68379a56
joaoKocssis <joaokocssis@gmail.com>
2024-03-19 12:00:27 -0300

    começando o repositorio

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSeries;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	var busca = "gilmore girls";
+		String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=68404664";
+		//https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=68404664
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(endereco);
+		System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
+		System.out.println(dados);
+	}
+}
